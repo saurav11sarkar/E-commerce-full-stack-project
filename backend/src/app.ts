8e3b43bfa241e3a2e923d84c28a9229bc73da997
saurav11sarkar <sarkarsaurav11@outlook.com>
2025-03-08 21:12:48 +0600

    single cart

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,7 +6,14 @@ import router from "./router/routes";
 
 const app = express();
 app.use(express.json({ limit: "25mb" }));
-app.use(cors({ origin: "http://localhost:5173/", credentials: true }));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
+// app.use(cors({ origin: "http://localhost:5173/", credentials: true }));
 // app.use(express.urlencoded({ limit: "25mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookiesParser());
--- frontend/src/pages/shop/productDetils/SingleProducts.jsx
+++ frontend/src/pages/shop/productDetils/SingleProducts.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import { Link, useParams } from "react-router";
 import RattingStars from "../../../components/RattingStars";
+import { useFetchProductByIdQuery } from "../../../redux/features/products/productApi";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../../redux/features/cart/cartSlice";
 
 const SingleProducts = () => {
   const { id } = useParams();
-  console.log(id)
+  const dispatch = useDispatch();
+
+  const { data, error, isLoading } = useFetchProductByIdQuery(id);
+
+  const product = data?.data;
+  const singleProduct = product?.result || {};
+  const productReviews = product?.reviews || [];
+
+  // console.log(singleProduct);
+  // console.log(productReviews);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Something went wrong! {error.message}</p>;
+
+  const handleAddToCart = (product) => {
+    dispatch(addToCart(product));
+  };
+
   return (
     <>
       <section className="section__container bg-[#f4e5ec] ">
@@ -18,7 +38,7 @@ const SingleProducts = () => {
             <Link to={"/shop"}>Shop</Link>
           </span>
           <i className="ri-arrow-right-s-line"></i>
-          <span className="hover:text-[#ed3849]">product name</span>
+          <span className="hover:text-[#ed3849]">{singleProduct.name}</span>
         </div>
       </section>
 
@@ -27,8 +47,8 @@ const SingleProducts = () => {
           {/* product img */}
           <div className="md:w-1/2 w-full">
             <img
-              src="https://images.unsplash.com/photo-1512201078372-9c6b2a0d528a?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt=""
+              src={singleProduct.image}
+              alt={singleProduct.name}
               className="rounded-md w-full h-auto"
             />
           </div>
@@ -37,33 +57,37 @@ const SingleProducts = () => {
               className="text-2xl font-semibold"
               style={{ marginBottom: "16px" }}
             >
-              Product Name
+              {singleProduct.name}
             </h3>
             <p
               className="text-xl text-[#ed3849]"
               style={{ marginBottom: "16px" }}
             >
-              $100 <s>$130</s>
+              ${singleProduct?.price}{" "}
+              {singleProduct?.oldPrice && <s>${singleProduct?.oldPrice}</s>}
             </p>
             <p className="text-gray-400" style={{ marginBottom: "16px" }}>
-              This is an product description
+              {singleProduct?.description}
             </p>
 
             {/* aditional product info */}
-            <div>
+            <div className="flex flex-col gap-2">
               <p>
-                <strong>Category:</strong> accessories
+                <strong>Category:</strong> {singleProduct?.category}
               </p>
               <p>
-                <strong>Color:</strong> beige
+                <strong>Color:</strong> {singleProduct?.color}
               </p>
               <div className="flex gap-1 items-center">
                 <strong>Ratting: </strong>
-                <RattingStars rating={"4"} />
+                <RattingStars rating={singleProduct?.rating} />
               </div>
             </div>
 
             <button
+              onClick={(e) => {
+                e.stopPropagation(), handleAddToCart(singleProduct);
+              }}
               className="  bg-[#ed3849] text-white rounded-md"
               style={{ marginTop: "1.5rem", padding: "8px 16px" }}
             >
@@ -75,7 +99,9 @@ const SingleProducts = () => {
 
       {/* display review */}
       {/* Todo */}
-      <section className="section__container" style={{marginTop:'2rem'}}>Review</section>
+      <section className="section__container" style={{ marginTop: "2rem" }}>
+        Review
+      </section>
     </>
   );
 };
